3ed54446b1d9f6ed7699d45204fec0fd89db39d5
Matheus Abegg <1006630+mpabegg@users.noreply.github.com>
2023-11-25 14:42:29 -0300

    Clean slate

---
+++ keymap_img/update_keymap_img.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+kb=$1
+
+if [ -z $kb ]; then
+  echo "Need to choose a keyboard"
+  exit 1
+fi
+
+echo 'Parsing ZMK keymap...'
+keymap -c config.yaml parse -c 10 -z "../config/${kb}.keymap" > "${kb}_keymap.yaml"
+
+echo '\n\nAdjusting keymap yaml...'
+# rm -rf chocofi_keymap.yamlupdated && ./keymap_img_adjuster.py chocofi_keymap.yaml
+
+echo '\n\nDrawing keymap...'
+keymap -c config.yaml draw -k "$kb" "${kb}_keymap.yaml" > "${kb}_keymap.svg"
+
+open "${kb}_keymap.svg"
---
+++ readme.md
@@ -0,0 +1,8 @@
+# mpa's zmk-config
+
+This is my personal [ZMK firmware](https://github.com/zmkfirmware/zmk/) configuration.
+
+I forked it from [urob](https://github.com/urob/zmk-config) and made some changes to suit my needs.
+I'm using my own fork of the ZMK firmware, in which I have meged the Chocofi definition.
+
+![](keymap_img/chocofi_keymap.svg)
