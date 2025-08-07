7eb43dd9460fb51c9325e4b1209d64456038327e
Matheus Abegg <1006630+mpabegg@users.noreply.github.com>
2025-08-07 15:50:05 -0300

    Update keymap image script to support CI mode and adjust GitHub Action workflow

--- keymap_img/update_keymap_img.sh
+++ keymap_img/update_keymap_img.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 
 kb=$1
+ci_mode=false
+
+# Parse arguments
+while [ $# -gt 0 ]; do
+  case $1 in
+    --ci)
+      ci_mode=true
+      shift
+      ;;
+    *)
+      kb=$1
+      shift
+      ;;
+  esac
+done
 
 if [ -z $kb ]; then
   echo "Need to choose a keyboard"
+  echo "Usage: $0 [--ci] <keyboard>"
   exit 1
 fi
 
@@ -18,4 +34,6 @@ fi
 keymap -c my_config.yaml parse -c 10 -z "../config/${kb}.keymap" > "${kb}_keymap.yaml"
 keymap -c my_config.yaml draw -k "$kb" "${kb}_keymap.yaml" > "${kb}_keymap.svg"
 
-open "${kb}_keymap.svg"
+if [ "$ci_mode" = false ]; then
+  open "${kb}_keymap.svg"
+fi
